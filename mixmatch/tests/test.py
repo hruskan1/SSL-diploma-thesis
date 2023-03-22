8bbccb3f551e8f9afc1c462314c20e73d65be9bb
Antonin Hruska <hruskan1@fel.cvut.cz>
2023-03-22 14:23:00 +0100

    Add added: kl_divergence test

--- mixmatch/tests/test.py
+++ mixmatch/tests/test.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("..")
 
 import mixmatch as mixmatch
+import losses as losses
 
 def test_shape_of_avg_predictions():
     """Test snippet of mixmatch averaging labels for classifications"""
@@ -63,12 +64,44 @@ def test_mixup():
     # print("Mixed-up labels shape:", mixed_labels.shape)
     # print("Mixing coefficients shape:", lam.shape)
     
+
+def test_kl_divergence():
+ 
+    # Define input and target tensors
+    input_tensor = torch.tensor([[1.0, 2.0, 3.0], [2.0, 3.0, 1.0]])
+    target_tensor = torch.tensor([[0.3, 0.3, 0.4], [0.2, 0.5, 0.3]])
+
+    # Compute KL divergence with default reduction
+    kl_div = losses.kl_divergence(input_tensor, target_tensor)
+
+    # Check that output is a scalar
+    
+    assert isinstance(kl_div, torch.Tensor)
+    assert kl_div.shape == torch.Size([])
+
+    # Compute KL divergence with reduction='none'
+    kl_div_none = losses.kl_divergence(input_tensor, target_tensor, reduction='none')
+
+    # Check that output is a tensor with the same shape as the input
+    assert isinstance(kl_div_none, torch.Tensor)
+    assert kl_div_none.shape == input_tensor.shape
+
+    # Check if two same predictions are equal
+    input_tensor = torch.tensor([[0, 1e10, 0], [1e10, 0, 0]])
+    target_tensor = torch.tensor([[0., 1., 0.], [1., 0., 0.]])
+    eps = 1e-3
+
+    kl_div = losses.kl_divergence(input_tensor,target_tensor)
+    assert kl_div < eps # kl_div always hiher then 0
+
     
 
 if __name__ == "__main__":
     #test_shape_of_avg_predictions()
     #test_sharpen()
-    test_mixup()
+    #test_mixup()
+    test_kl_divergence()
+
 
 
 
