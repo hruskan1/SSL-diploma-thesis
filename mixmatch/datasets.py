383029e77c73caaec22012ee901ae6435c789a75
Antonin Hruska <hruskan1@fel.cvut.cz>
2023-03-22 12:35:03 +0100

    Add added CIFAR10Labeled class added CIFAR10Unlabeled class added train_test_val_split

--- mixmatch/datasets.py
+++ mixmatch/datasets.py
@@ -6,17 +6,20 @@ https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 """
 
 import os
+import numpy as np
 import torch
 from torch.utils import data
+from sklearn.model_selection import train_test_split
 
-from torchvision import transforms
+import torchvision
+from torchvision.transforms import functional as TF
 from torchvision.datasets.folder import (
     IMG_EXTENSIONS,
     default_loader,
     has_file_allowed_extension,
 )
-from torchvision.transforms import functional as TF
-from typing import Tuple
+
+from typing import Tuple,Optional,Callable,Any,Union
 
 
 # Code retaken from  https://github.com/Jonas1312/pytorch-segmentation-dataset and 
@@ -113,6 +116,103 @@ class SegmentationDatasetLabeled(SegmentationDatasetUnlabeled):
         assert len(img_names) == len(mask_names)
 
         return img_names, mask_names
+
+
+class CIFAR10Labeled(torchvision.datasets.CIFAR10):
+    def __init__(self, 
+                root:str,
+                indicies:Optional[torch.Tensor]=None,
+                train:bool=True,
+                transform:Optional[Callable]=None,
+                target_transform:Optional[Callable]=None,
+                download:Optional[bool]=False)->None:
+        """
+        Module based on CIFAR 10 dataset https://pytorch.org/vision/main/generated/torchvision.datasets.CIFAR10.html
+        The data are kept as a numpy array
+        The targets are parsed into the torch.tensor with dtype=long
+        Args:
+            indicies (Optional[torch.Tensor]): Indicies, which selects the subset of the original CIFAR10 dataset 
+                (if None, the whole dataset is taken)
+            rest: see original documentation
+        """
+
+        super(CIFAR10Labeled, self).__init__(root, train, transform, target_transform, download)
+
+        self.targets = torch.Tensor(self.targets).to(torch.long)
+
+        if indicies is not None:
+            indicies = indicies.round().to(torch.long)
+            self.data = self.data[indicies]
+            self.targets = self.targets[indicies]
+
+
+class CIFAR10Unlabeled(CIFAR10Labeled):
+    def __init__(self, 
+                root:str,
+                indicies:Optional[torch.Tensor]=None,
+                train:bool=True,
+                transform:Optional[Callable]=None,
+                target_transform:Optional[Callable]=None,
+                download:Optional[bool]=False)->None:
+        """
+        Module based on CIFAR 10 dataset https://pytorch.org/vision/main/generated/torchvision.datasets.CIFAR10.html
+        The data are kept as a numpy array
+        The targets are parsed into the torch.tensor with dtype=long (for debugging purposes)
+        but are not obtained by __getitem__ method. 
+        Args:
+            indicies (Optional[torch.Tensor]): Indicies, which selects the subset of the original CIFAR10 dataset 
+                (if None, the whole dataset is taken)
+            rest: see original documentation
+        """
+        
+
+        super(CIFAR10Unlabeled, self).__init__(root,indicies, train, transform, target_transform, download)
+
+    
+    def __getitem__(self, index: int) -> Any:
+        """Returns transofrmed image"""
+        img,_ = super().__getitem__(index)
+        return img 
+
+        
+def train_test_val_split(arrays:list[Union[list,np.ndarray]],split:Tuple[float,float]=(0.2, 0.1), shuffle=True,random_state=None,stratify_index:Optional[int]=None):
+    """Split each of arrays (dataset,labels) with ratio of  (1-split[0]-split[1]) / split[0] /split[1] 
+    into train,test,val sets respectively. 
+    Wrapper around https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    
+    Args:
+      arrays: list of indexables with same length / shape[0]
+      split (Tuple[float,float]): tuple of split ratio in `test,val` order.
+      stratify_index (int) : index which selects member of arrays according to which we stratify 
+      (Therefore stratification mask has to be part of arrays)
+      rest: See original documentation
+      
+    Returns:
+      splitting: list, length= 3*len(arrays), with order: train,test,val.
+    """
+    stratify = arrays[stratify_index] if stratify_index is not None else None
+
+    ret = train_test_split(*arrays, 
+                           test_size=(split[0]+split[1]),
+                           shuffle=shuffle,
+                           random_state=random_state,
+                           stratify=stratify)
+    
+    train_ = [ret[i] for i in range(0,len(ret),2)] # select the training / non-training part
+    rest_  = [ret[i] for i in range(1,len(ret),2)]  
+    
+    
+    ret = train_test_split(*rest_,
+                           test_size=(split[1] / (split[0]+split[1])),
+                           shuffle=True,
+                           random_state=random_state,
+                           stratify=stratify)
+    
+    test_ = [ret[i] for i in range(0,len(ret),2)] 
+    val_  = [ret[i] for i in  range(1,len(ret),2)] 
+    return *train_, *test_, *val_ 
+
+         
 
 
 
