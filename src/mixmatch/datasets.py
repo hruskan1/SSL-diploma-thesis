8937bf24962c9ed2e3273ade41681028caa3af21
Antonin Hruska <hruskan1@fel.cvut.cz>
2023-05-09 13:12:41 +0200

    Add change default number of classes to 27.

--- src/mixmatch/datasets.py
+++ src/mixmatch/datasets.py
@@ -211,33 +211,33 @@ class CityScapeDataset(torchvision.datasets.Cityscapes):
         CityscapesClass("dynamic",              5,        0,  "void",               0,          False,      True,    (111,  74,   0)),
         CityscapesClass("ground",               6,        0,  "void",               0,          False,      True,    ( 81,   0,  81)),
         CityscapesClass("road",                 7,        1,  "flat",               1,          False,      False,   (128,  64, 128)),
-        CityscapesClass("sidewalk",             8,        1,  "flat",               1,          False,      False,   (244,  35, 232)),
-        CityscapesClass("parking",              9,        1,  "flat",               1,          False,      True,    (250, 170, 160)),
-        CityscapesClass("rail track",           10,       1,  "flat",               1,          False,      True,    (230, 150, 140)),
-        CityscapesClass("building",             11,       2,  "construction",       2,          False,      False,   ( 70,  70,  70)),
-        CityscapesClass("wall",                 12,       2,  "construction",       2,          False,      False,   (102, 102, 156)),
-        CityscapesClass("fence",                13,       2,  "construction",       2,          False,      False,   (190, 153, 153)),
-        CityscapesClass("guard rail",           14,       2,  "construction",       2,          False,      True,    (180, 165, 180)),
-        CityscapesClass("bridge",               15,       2,  "construction",       2,          False,      True,    (150, 100, 100)),
-        CityscapesClass("tunnel",               16,       2,  "construction",       2,          False,      True,    (150, 120,  90)),
-        CityscapesClass("pole",                 17,       3,  "object",             3,          False,      False,   (153, 153, 153)),
-        CityscapesClass("polegroup",            18,       3,  "object",             3,          False,      True,    (153, 153, 153)),
-        CityscapesClass("traffic light",        19,       3,  "object",             3,          False,      False,   (250, 170,  30)),
-        CityscapesClass("traffic sign",         20,       3,  "object",             3,          False,      False,   (220, 220,   0)),
-        CityscapesClass("vegetation",           21,       4,  "nature",             4,          False,      False,   (107, 142,  35)),
-        CityscapesClass("terrain",              22,       4,  "nature",             4,          False,      False,   (152, 251, 152)),
-        CityscapesClass("sky",                  23,       5,  "sky",                5,          False,      False,   ( 70, 130, 180)),
-        CityscapesClass("person",               24,       6,  "human",              6,          True,       False,   (220,  20,  60)),
-        CityscapesClass("rider",                25,       6,  "human",              6,          True,       False,   (255,   0,   0)),
-        CityscapesClass("car",                  26,       7,  "vehicle",            7,          True,       False,   (  0,   0, 142)),
-        CityscapesClass("truck",                27,       7,  "vehicle",            7,          True,       False,   (  0,   0,  70)),
-        CityscapesClass("bus",                  28,       7,  "vehicle",            7,          True,       False,   (  0,  60, 100)),
-        CityscapesClass("caravan",              29,       7,  "vehicle",            7,          True,       True,    (  0,   0,  90)),
-        CityscapesClass("trailer",              30,       7,  "vehicle",            7,          True,       True,    (  0,   0, 110)),
-        CityscapesClass("train",                31,       7,  "vehicle",            7,          True,       False,   (  0,  80, 100)),
-        CityscapesClass("motorcycle",           32,       7,  "vehicle",            7,          True,       False,   (  0,   0, 230)),
-        CityscapesClass("bicycle",              33,       7,  "vehicle",            7,          True,       False,   (119,  11,  32)),
-        CityscapesClass("license plate",        -1,       7,  "vehicle",            7,          False,      True,    (  0,   0, 142)),
+        CityscapesClass("sidewalk",             8,        2,  "flat",               1,          False,      False,   (244,  35, 232)),
+        CityscapesClass("parking",              9,        3,  "flat",               1,          False,      True,    (250, 170, 160)),
+        CityscapesClass("rail track",           10,       4,  "flat",               1,          False,      True,    (230, 150, 140)),
+        CityscapesClass("building",             11,       5,  "construction",       2,          False,      False,   ( 70,  70,  70)),
+        CityscapesClass("wall",                 12,       6,  "construction",       2,          False,      False,   (102, 102, 156)),
+        CityscapesClass("fence",                13,       7,  "construction",       2,          False,      False,   (190, 153, 153)),
+        CityscapesClass("guard rail",           14,       8,  "construction",       2,          False,      True,    (180, 165, 180)),
+        CityscapesClass("bridge",               15,       9,  "construction",       2,          False,      True,    (150, 100, 100)),
+        CityscapesClass("tunnel",               16,       10, "construction",       2,          False,      True,    (150, 120,  90)),
+        CityscapesClass("pole",                 17,       11, "object",             3,          False,      False,   (153, 153, 153)),
+        CityscapesClass("polegroup",            18,       12, "object",             3,          False,      True,    (153, 153, 153)),
+        CityscapesClass("traffic light",        19,       13, "object",             3,          False,      False,   (250, 170,  30)),
+        CityscapesClass("traffic sign",         20,       14, "object",             3,          False,      False,   (220, 220,   0)),
+        CityscapesClass("vegetation",           21,       15, "nature",             4,          False,      False,   (107, 142,  35)),
+        CityscapesClass("terrain",              22,       16, "nature",             4,          False,      False,   (152, 251, 152)),
+        CityscapesClass("sky",                  23,       17, "sky",                5,          False,      False,   ( 70, 130, 180)),
+        CityscapesClass("person",               24,       18, "human",              6,          True,       False,   (220,  20,  60)),
+        CityscapesClass("rider",                25,       19, "human",              6,          True,       False,   (255,   0,   0)),
+        CityscapesClass("car",                  26,       20, "vehicle",            7,          True,       False,   (  0,   0, 142)),
+        CityscapesClass("truck",                27,       21, "vehicle",            7,          True,       False,   (  0,   0,  70)),
+        CityscapesClass("bus",                  28,       22, "vehicle",            7,          True,       False,   (  0,  60, 100)),
+        CityscapesClass("caravan",              29,       23, "vehicle",            7,          True,       True,    (  0,   0,  90)),
+        CityscapesClass("trailer",              30,       24, "vehicle",            7,          True,       True,    (  0,   0, 110)),
+        CityscapesClass("train",                31,       25, "vehicle",            7,          True,       False,   (  0,  80, 100)),
+        CityscapesClass("motorcycle",           32,       26, "vehicle",            7,          True,       False,   (  0,   0, 230)),
+        CityscapesClass("bicycle",              33,       27, "vehicle",            7,          True,       False,   (119,  11,  32)),
+        CityscapesClass("license plate",        -1,       0,  "vehicle",            7,          False,      True,    (  0,   0, 142)),
     ]
 
     id2trainid = {label.id: label.train_id for label in classes}
@@ -245,9 +245,11 @@ class CityScapeDataset(torchvision.datasets.Cityscapes):
     trainid2names = {label.train_id : label.name for label in reversed(classes)}
     num_classes = len(trainid2color.keys()) #- 1 #remove one if license_plate has '-1' (not valid class)
     
-    class_weights = [0,1,1,1,1,1,1,1] # ignore 0 class (unlabeled)
-    custom_class_weights = [0,1,1,5,3,1,5,2] #manually twiddle class weights
-    # for mode == fine 
+
+    class_weights = [1] * num_classes
+    class_weights[0] = 0 # ignore 0 class (unlabeled)
+
+    # std & mean for mode == fine 
     mean = torch.Tensor([0.485, 0.456, 0.406]) 
     std = torch.Tensor([0.229, 0.224, 0.225])
 
@@ -623,31 +625,35 @@ def get_CityScape(root:str,n_labeled:int,n_val:int,batch_size:int,mode:str='fine
     # if mode == 'fine', then sizes of  train/val/test is 2975/500/1525
     # if mode == 'corse' then sizes of train/val/train_extra is 2975/500/19998 
     num_examples = len(base_dataset)
-   
 
-    if n_val > 0:
-        indicies_unlabeled,indicies_labeled,indicies_val = train_test_val_split([range(num_examples)],
-                                                                                        split = (n_labeled/num_examples,n_val/num_examples),
-                                                                                        shuffle=True,
-                                                                                        random_state=None,
-                                                                                        stratify_index=None)
+    if n_labeled >= num_examples: 
+        unlabeled_ = CityScapeDataset(root,split = 'train',mode = mode,target_type = target_type,transform = _trans,target_transform = _target_trans)
+        labeled_ = CityScapeDataset(root,split = 'train',mode = mode,target_type = target_type,transform = _trans,target_transform = _target_trans)
+        validation_dataloader,val_ = None,None # canot use n_val as they are dependent on training set
+    else: # n_labeled < num_examples
+        if n_val > 0:
+            indicies_unlabeled,indicies_labeled,indicies_val = train_test_val_split([range(num_examples)],
+                                                                                            split = (n_labeled/num_examples,n_val/num_examples),
+                                                                                            shuffle=True,
+                                                                                            random_state=None,
+                                                                                            stratify_index=None)
+            
+            val_ = data.Subset(base_dataset,indicies_val)
         
-        val_ = data.Subset(base_dataset,indicies_val)
-    
-        validation_dataloader = data.DataLoader(val_,batch_size,shuffle=False,num_workers=num_workers,)
+            validation_dataloader = data.DataLoader(val_,batch_size,shuffle=False,num_workers=num_workers,)
 
-    else: # n_val == 0
-        indicies_unlabeled,indicies_labeled  = train_test_split(range(num_examples), 
-                                                        test_size=n_labeled/num_examples,
-                                                        shuffle=True,
-                                                        random_state=None,
-                                                        stratify=None)
-        validation_dataloader,val_ = None,None
+        else: # n_val == 0
+            indicies_unlabeled,indicies_labeled  = train_test_split(range(num_examples), 
+                                                            test_size=n_labeled/num_examples,
+                                                            shuffle=True,
+                                                            random_state=None,
+                                                            stratify=None)
+            validation_dataloader,val_ = None,None
         
-    unlabeled_ = data.Subset(base_dataset,indicies_unlabeled) # TODO: DEBUG PURPOSES PUT IT BACK TO UNLABELED LATER
-     #unlabeled_ = UnlabeledDataset(data.Subset(base_dataset,indicies_unlabeled))
-    labeled_ = data.Subset(base_dataset,indicies_labeled)
+        unlabeled_ = data.Subset(base_dataset,indicies_unlabeled) 
+        labeled_ = data.Subset(base_dataset,indicies_labeled)
 
+    # unlabeled_ = UnlabeledDataset(unlabeled_) # TODO: DEBUG PURPOSES PUT IT BACK TO UNLABELED LATER
     test_ = CityScapeDataset(root,
                              split='val',
                              mode=mode,
